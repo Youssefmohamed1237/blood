5f36f01e26156ae2315e76b884844bca992b10aa
Youssefmohamed1237 <118205824+Youssefmohamed1237@users.noreply.github.com>
2024-06-21 02:08:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const compression = require("compression");
 const globalErrorHandler = require("./controller/globalcontroller");
 const donorroute = require("./routs/donorrouter");
 const needroute = require("./routs/needrouter");
+const placeroute = require("./routs/placerouter");
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(compression());
 app.use("/api/donor", donorroute);
 app.use("/api/need", needroute);
+app.use("/api/place", placeroute);
 app.use(globalErrorHandler);
 module.exports = app;
+
