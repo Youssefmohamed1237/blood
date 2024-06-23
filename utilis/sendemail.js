9e7e1841dbc69d25fbc9a72bb333cfbe11d1bb71
Youssefmohamed1237 <118205824+Youssefmohamed1237@users.noreply.github.com>
2024-06-23 21:25:34 +0300

    Update sendemail.js

--- utilis/sendemail.js
+++ utilis/sendemail.js
@@ -1,19 +1,19 @@
-// const nodemailer = require("nodemailer");
-// const sendEmail = async (options) => {
-//   const transporter = nodemailer.createTransport({
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//   });
-//   const mailoptions = {
-//     from: "reViVe blood bank<revive@gmail.com>",
-//     to: options.email,
-//     subject: options.subject,
-//     text: options.message,
-//   };
-//   await transporter.sendMail(mailoptions);
-// };
-// module.exports = sendEmail;
+const nodemailer = require("nodemailer");
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  const mailoptions = {
+    from: "reViVe blood bank<revive@gmail.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  await transporter.sendMail(mailoptions);
+};
+module.exports = sendEmail;
