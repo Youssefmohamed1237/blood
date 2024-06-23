68c5845f318c646fbf2fafc05e634c795edf4500
Youssefmohamed1237 <118205824+Youssefmohamed1237@users.noreply.github.com>
2024-06-23 18:42:33 +0300

    Update needModel.js

--- model/needModel.js
+++ model/needModel.js
@@ -1,36 +1,46 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const needschema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    // unique: true,
-    validate: [validator.isEmail, "email should be valid"],
-  },
   name: {
     type: String,
     required: [true, "please enter the name"],
   },
-  profileImg: {
-    type: String,
-    // required: [true, "profile image is required"],
-  },
-
-  phone: {
+  email: {
     type: String,
     required: true,
+    validate: [validator.isEmail, "email should be valid"],
+  },
+  hospitalname: {
+    type: String,
+    required: [true, "hospital name enter the name"],
+  },
+
+  bookingdate: {
+    type: Date,
+    required: [true, "booking date is required"],
     validate: {
-      validator: function (v) {
-        return validator.isMobilePhone(v, "ar-EG");
+      validator: function (el) {
+        return el > Date.now();
       },
-      message: "not valid  mobile phone",
+      message: "not suitable date",
     },
   },
+
+  // phone: {
+  //   type: String,
+  //   required: true,
+  //   validate: {
+  //     validator: function (v) {
+  //       return validator.isMobilePhone(v, "ar-EG");
+  //     },
+  //     message: "not valid  mobile phone",
+  //   },
+  // },
   bloodType: {
     type: String,
     required: true,
     enum: {
-      values: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
+      values: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-", "unknown"],
       message: "blood type is not sutiable",
     },
   },
@@ -39,15 +49,15 @@ const needschema = new mongoose.Schema({
     required: [true, "city is required"],
   },
 
-  message: {
-    type: String,
-  },
+  // message: {
+  //   type: String,
+  // },
 });
-needschema.post("init", (doc) => {
-  if (doc.profileImg) {
-    const imagurl = `${process.env.BASE_URL}/need/${doc.profileImg}`;
-    doc.profileImg = imagurl;
-  }
-});
+// needschema.post("init", (doc) => {
+//   if (doc.profileImg) {
+//     const imagurl = `${process.env.BASE_URL}/need/${doc.profileImg}`;
+//     doc.profileImg = imagurl;
+//   }
+// });
 const need = mongoose.model("need", needschema);
 module.exports = need;
